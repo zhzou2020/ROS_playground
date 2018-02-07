23cb9fe8508e0077ea5e4d5a657120e0c43c380f
zhuojin <zzjstudy@gmail.com>
2018-02-07 15:48:08 +0800

    add codes for aiml project

---
+++ ros_aiml/scripts/aiml_speechreco_client.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+import rospy
+from std_msgs.msg import String
+
+rospy.init_node('aiml_speechreco_client')
+
+pub = rospy.Publisher('chatter', String, queue_size=10)
+r = rospy.Rate(10)
+
+def get_speech(data):
+	speech_text = data.data
+	rospy.loginfo("I said:: %s", speech_text)
+	pub.publish(speech_text)	
+
+def listener():
+	rospy.loginfo("Starting Speech Recognition")
+	rospy.Subscriber("/recognizer/output", String, get_speech)
+	rospy.spin()
+
+while not rospy.is_shutdown():
+	listener()
+
